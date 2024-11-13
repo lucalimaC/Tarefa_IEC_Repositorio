56e328355df7577617018c3ca86565490dcbf3a9
lucalimaC <lucascamargo.aluno@unipampa.edu.br>
2024-11-12 22:15:08 -0300

    Add files via upload

---
+++ codigo_fonte.py
@@ -0,0 +1,11 @@
+print("Bem-vindo ao programa de soma!")
+
+# Solicita dois números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realiza a soma
+soma = num1 + num2
+
+# Exibe o resultado
+print("A soma de", num1, "e", num2, "é:", soma)
